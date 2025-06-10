85a4da8f12c14d038ad7c908ebad291154514379
Sai Phanindra Kothuri <kothuriphanindra1999@gmail.com>
2025-06-10 11:15:46 -0400

    Solving Remove Nth Node From End Of List problem in LeetCode

---
+++ src/edu/charlotte/data_structures/linked_list/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,66 @@
+package edu.charlotte.data_structures.linked_list;
+
+public class RemoveNthNodeFromEndOfList {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode right = head;
+        ListNode left = dummy;
+
+        while(n > 0 && right != null) {
+            right = right.next;
+            n--;
+        }
+
+        while(right != null) {
+            left = left.next;
+            right = right.next;
+        }
+
+        left.next = left.next.next;
+        return dummy.next;
+    }
+
+    private ListNode createList(int[] arrayInput) {
+        ListNode dummy = new ListNode();
+        ListNode tail = dummy;
+        for(int input: arrayInput) {
+            tail.next = new ListNode(input, null);
+            tail = tail.next;
+        }
+        return dummy.next;
+    }
+
+    private void print(ListNode outputList) {
+        System.out.print("[");
+        while(outputList != null) {
+            System.out.print(outputList.val);
+            if(outputList.next != null)
+                System.out.print(" ");
+            outputList = outputList.next;
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String[] args) {
+        RemoveNthNodeFromEndOfList removeNthNodeFromEndOfList = new RemoveNthNodeFromEndOfList();
+        ListNode list1 = removeNthNodeFromEndOfList.createList(new int[]{1, 2, 3, 4, 5});
+        removeNthNodeFromEndOfList.print(removeNthNodeFromEndOfList.removeNthFromEnd(list1, 2));
+        list1 = removeNthNodeFromEndOfList.createList(new int[]{1});
+        removeNthNodeFromEndOfList.print(removeNthNodeFromEndOfList.removeNthFromEnd(list1, 1));
+        list1 = removeNthNodeFromEndOfList.createList(new int[]{1, 2});
+        removeNthNodeFromEndOfList.print(removeNthNodeFromEndOfList.removeNthFromEnd(list1, 1));
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+        public ListNode() {}
+        public ListNode(int val) {
+            this.val = val;
+        }
+        public ListNode(int val, ListNode next) {
+            this.val = val; this.next = next;
+        }
+    }
+}
