27e501915b251c3082148526cd00de45aea06be3
Phanindra08 <kothuriphanindra1999@gmail.com>
2025-07-26 22:00:09 -0400

    Adding a new solution for the LeetCode Arrays Problem BestTimeToBuyAndSellStock

--- src/edu/charlotte/data_structures/arrays/BestTimeToBuyAndSellStock.java
+++ src/edu/charlotte/data_structures/arrays/BestTimeToBuyAndSellStock.java
@@ -10,7 +10,7 @@ public class BestTimeToBuyAndSellStock {
      * @param prices - An array of integers indicating the stock price on each day.
      * @return - returns an integer which indicates maximum profit that can be achieved from a single buy and sell transaction. Returns 0 if no profit is possible.
      */
-    public int maxProfit(int[] prices) {
+    public int maxProfitSolution1(int[] prices) {
         int maximumProfit = 0;
         int leftPointer = 0, rightPointer = 1;
         while(rightPointer < prices.length) {
@@ -23,13 +23,28 @@ public class BestTimeToBuyAndSellStock {
         return maximumProfit;
     }
 
+    public int maxProfitSolution2(int[] prices) {
+        int difference = 0, min = prices[prices.length - 1], max = prices[prices.length - 1];
+        for (int index = prices.length - 2; index >= 0; index--) {
+            if (prices[index] < max && prices[index] < min) {
+                min = prices[index];
+                if(max - min > difference)
+                    difference = max - min;
+            } else if (prices[index] > max) {
+                min = prices[index];
+                max = prices[index];
+            }
+        }
+        return difference;
+    }
+
     /**
      * We have given the Sample test cases from the Leetcode as input to the {@code maxProfit} method.
      * @param args - Command line arguments (not used)
      */
     public static void main(String[] args) {
         BestTimeToBuyAndSellStock bestTimeToBuyAndSellStockObject = new BestTimeToBuyAndSellStock();
-        System.out.println(bestTimeToBuyAndSellStockObject.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
-        System.out.println(bestTimeToBuyAndSellStockObject.maxProfit(new int[]{7, 6, 4, 3, 1}));
+        System.out.println(bestTimeToBuyAndSellStockObject.maxProfitSolution1(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(bestTimeToBuyAndSellStockObject.maxProfitSolution2(new int[]{7, 6, 4, 3, 1}));
     }
 }
