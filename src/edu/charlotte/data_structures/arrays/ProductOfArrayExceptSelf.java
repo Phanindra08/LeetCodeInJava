23145864b3145e1114faed4f77a316ff75ff18a0
Phanindra08 <kothuriphanindra1999@gmail.com>
2025-07-27 18:11:10 -0400

    Adding another Solution to LeetCode Arrays problem Product Of Array Except Self

--- src/edu/charlotte/data_structures/arrays/ProductOfArrayExceptSelf.java
+++ src/edu/charlotte/data_structures/arrays/ProductOfArrayExceptSelf.java
@@ -3,7 +3,7 @@ package edu.charlotte.data_structures.arrays;
 import java.util.Arrays;
 
 public class ProductOfArrayExceptSelf {
-    public int[] productExceptSelf(int[] nums) {
+    public int[] productExceptSelfSolution1(int[] nums) {
         int suffix = 1;
         int[] answer = new int[nums.length];
         answer[0] = 1;
@@ -16,9 +16,26 @@ public class ProductOfArrayExceptSelf {
         return answer;
     }
 
+    public int[] productExceptSelfSolution2(int[] nums) {
+        int[] answer = new int[nums.length];
+        int product = 1;
+        for(int index = 0; index < nums.length; index++) {
+            answer[index] = product;
+            product *= nums[index];
+        }
+
+        product = 1;
+        for(int index = nums.length - 1; index >= 0; index--) {
+            answer[index] = answer[index] * product;
+            product *= nums[index];
+        }
+
+        return answer;
+    }
+
     public static void main(String[] args) {
         ProductOfArrayExceptSelf productOfArrayExceptSelf = new ProductOfArrayExceptSelf();
-        System.out.println(Arrays.toString(productOfArrayExceptSelf.productExceptSelf(new int[]{1, 2, 3, 4})));
-        System.out.println(Arrays.toString(productOfArrayExceptSelf.productExceptSelf(new int[]{-1, 1, 0, -3, 3})));
+        System.out.println(Arrays.toString(productOfArrayExceptSelf.productExceptSelfSolution1(new int[]{1, 2, 3, 4})));
+        System.out.println(Arrays.toString(productOfArrayExceptSelf.productExceptSelfSolution2(new int[]{-1, 1, 0, -3, 3})));
     }
 }
