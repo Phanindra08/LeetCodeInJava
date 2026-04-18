f6cc3c39316ebbefc6b0b2546b0adfd8d6028c8d
Phanindra08 <kothuriphanindra1999@gmail.com>
2026-04-18 19:35:43 -0400

    Adding 2 more Solutions to the TwoSum class in Arrays Concept

--- src/edu/charlotte/data_structures/arrays/TwoSum.java
+++ src/edu/charlotte/data_structures/arrays/TwoSum.java
@@ -13,14 +13,38 @@ public class TwoSum {
      * @param target - The target sum that the two numbers in the array should add up.
      * @return - returns an array containing the indices of the two numbers whose sum is equal to the target. It returns null if no such pair exists.
      */
-    public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+    public int[] twoSumSolution1(int[] nums, int target) {
+        Map<Integer, Integer> previousMap = new HashMap<>();
         for(int index = 0; index < nums.length; index++) {
-            if(map.containsKey(target - nums[index]))
-                return new int[] {map.get(target - nums[index]), index};
-            map.put(nums[index], index);
+            if(previousMap.containsKey(target - nums[index]))
+                return new int[] {previousMap.get(target - nums[index]), index};
+            previousMap.put(nums[index], index);
         }
         return null;
+    }
+
+    // Brute-Force Approach 1
+    public int[] twoSumSolution2(int[] nums, int target) {
+        for(int index1 = 1; index1 < nums.length; index1++){
+            for(int index2 = index1; index2 < nums.length; index2++){
+                if(nums[index2] + nums[index2 - index1] == target){
+                    return new int[] {index2 - index1, index2};
+                }
+            }
+        }
+        return null;
+    }
+
+    // Brute-Force Approach 2
+    public int[] twoSumSolution3(int[] nums, int target) {
+        for(int index1 = 0; index1 < nums.length - 1; index1++) {
+            for(int index2 = index1 + 1;index2 < nums.length; index2++) {
+                if(nums[index1] + nums[index2] == target) {
+                    return new int[] {index1, index2};
+                }
+            }
+        }
+        return new int[]{};
     }
 
     /**
@@ -29,8 +53,8 @@ public class TwoSum {
      */
     public static void main(String[] args) {
         TwoSum twoSumObject = new TwoSum();
-        System.out.println(Arrays.toString(twoSumObject.twoSum(new int[]{2,7,11,15}, 9)));
-        System.out.println(Arrays.toString(twoSumObject.twoSum(new int[]{3,2,4}, 6)));
-        System.out.println(Arrays.toString(twoSumObject.twoSum(new int[]{3,3}, 6)));
+        System.out.println(Arrays.toString(twoSumObject.twoSumSolution1(new int[]{2,7,11,15}, 9)));
+        System.out.println(Arrays.toString(twoSumObject.twoSumSolution2(new int[]{3,2,4}, 6)));
+        System.out.println(Arrays.toString(twoSumObject.twoSumSolution3(new int[]{3,3}, 6)));
     }
 }
