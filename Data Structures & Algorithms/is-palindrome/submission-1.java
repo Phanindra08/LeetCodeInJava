fdb068ce45293928dacf37236fe0f11740d62def
Sai Phanindra Kothuri <kothuriphanindra1999@gmail.com>
2026-04-25 07:52:48 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        StringBuilder s1 = new StringBuilder();
+        int length = s.length();
+        int unicodeOfCharacter;
+        for(int index = 0; index < length; index++) {
+            unicodeOfCharacter = s.codePointAt(index);
+            if((unicodeOfCharacter >= 65 && unicodeOfCharacter <= 90) || 
+            (unicodeOfCharacter >= 97 && unicodeOfCharacter <= 122) || 
+            (unicodeOfCharacter >= 48 && unicodeOfCharacter <= 57))
+                s1.append(s.charAt(index));
+        }
+
+        length = s1.length();
+        for(int index = 0; index < length; index++) {
+            if(s1.charAt(index) != s1.charAt(length - index - 1))
+                return false;
+        }
+        return true;
+    }
+}
